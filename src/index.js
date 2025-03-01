5abfde40bc64f2e3795f5aa78a2b6630ea2dcb48
papiezonator <52249311+papiezonator@users.noreply.github.com>
2025-03-01 21:08:06 +0100

    added an auto generated dialog form

--- src/index.js
+++ src/index.js
@@ -1,2 +1,100 @@
 import "./styles.css";
 
+window.addEventListener("load", () => {
+    createBtn();
+    createDialog();
+    createForm();
+})
+
+//todo item class
+class Item {
+    constructor(name, desc){
+        this.name = name;
+        this.desc = desc;
+    }
+}
+
+//creating btn for triggering dialog
+const createBtn = () =>{
+    const headerWrapper = document.querySelector("#header-wrapper");
+    const btn = document.createElement("button");
+    headerWrapper.appendChild(btn);
+    btn.id = "addBtn";
+    btn.innerHTML = "Add item"
+    addFunction(btn);
+}
+
+//creating the dialog
+const createDialog = () => {
+    const headerWrapper = document.querySelector("#header-wrapper");
+    const dialog = document.createElement("dialog");
+    headerWrapper.appendChild(dialog);
+    dialog.id = "dialog";
+    return;
+}
+
+//creating the form inside dialog
+const createForm = () => {
+    const dialog = document.querySelector("#dialog");
+    const form = document.createElement("form");
+    dialog.appendChild(form);
+    form.method = "dialog";
+    addFormItems(form);
+    return;
+}
+
+//adding form children
+const addFormItems = (form) => {
+    for(let i = 0; i < 3; i++){
+        const div = document.createElement("div");
+        const label = document.createElement("label");
+        const input = document.createElement("input");
+        form.appendChild(div);
+        div.appendChild(label);
+        div.appendChild(input);
+        switch(i){
+            case 0:
+                label.innerHTML = `name:`;
+                label.for = "name";
+                input.type = "text";
+                input.name = "name";
+                input.id = "nameBtn";
+                break;
+            case 1:
+                label.innerHTML = `desc:`;
+                label.for = "desc";
+                input.type = "text";
+                input.name = "desc";
+                input.id = "descBtn";
+                break;
+            case 2:
+                input.type = "submit";
+                input.name = "submit";
+                input.id = "submit";
+                addInput(input);
+                break;
+
+        }
+    }
+    return
+}
+
+const addFunction = (btn) => {
+    btn.addEventListener("click", () => {
+        const dialog = document.querySelector("#dialog");
+        dialog.showModal();
+    })
+}
+
+const addInput = (input) => {
+    input.addEventListener("click", () => {
+        showItem();
+    })
+}
+
+const showItem = () => {
+    const name = document.querySelector("#nameBtn");
+    const desc = document.querySelector("#descBtn");
+    let item = new Item(name.value, desc.value);
+    console.log(item);
+}
